cca81e188315f919c05b996ecafca3cd9834e112
dev-welli <welli.abdullah@xero.com>
2018-07-15 21:16:01 +1200

    add computerChoice function

--- main.js
+++ main.js
@@ -11,8 +11,18 @@ const paper_div = document.getElementById("paper");
 const scissor_div = document.getElementById("scissor");
 
 function game(userChoice){
-   console.log("BLAH " + userChoice);
+   const computerChoice = getComputerChoice();
+   console.log("user choice: " + userChoice);
+   console.log("computer choice: " +  computerChoice);
 }
+
+function getComputerChoice(){
+   const choices = ['rock', 'paper', 'scissor'];
+   const randomNum = Math.floor(Math.random() * 3 )//generating rounded random number between 0..2
+   return choices[randomNum];
+}
+
+
 
 function main(){
    rock_div.addEventListener('click', function(){
