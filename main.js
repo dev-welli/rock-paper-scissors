e4811c1e493ad0955eac37a02f15f3cc4243264d
dev-welli <welli.abdullah@xero.com>
2018-07-15 22:00:05 +1200

    add scores & status

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 //caching the DOM elements
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissor_div = document.getElementById("scissor");
@@ -16,26 +16,47 @@ function game(userChoice){
    //console.log("computer choice: " +  computerChoice);
 
    switch (userChoice + computerChoice){
-      case "rockscissor":
-      case "paperrock":
-      case "scissorpaper":
-         console.log("USER WINS");
+      case "RockScissor":
+      case "PaperRock":
+      case "ScissorPaper":
+         win(userChoice, computerChoice);
          break;
-      case "rockpaper":
-      case "paperscissor":
-      case "scissorrock":
-         console.log("COMPUTER WINS")
+      case "RockPaper":
+      case "PaperScissor":
+      case "ScissorRock":
+         lose(userChoice, computerChoice);
          break;
-      case "rockrock":
-      case "paperpaper":
-      case "scissorscissor":
-         console.log("DRAW!")
+      case "RockRock":
+      case "PaperPaper":
+      case "ScissorScissor":
+         draw(userChoice, computerChoice);
          break;
    }
 }
 
+function win(userChoice, computerChoice){
+   console.log("USER WINS");
+   userScore++;
+   console.log(userScore);
+   userScore_span.innerHTML = userScore;
+   result_p.innerHTML = userChoice + " beats " + computerChoice +". You win!";
+}
+
+function lose(userChoice, computerChoice){
+   console.log("COMPUTER WINS")
+   computerScore++;
+   console.log(computerScore);
+   computerScore_span.innerHTML = computerScore;
+   result_p.innerHTML = userChoice + " loses to " + computerChoice +". You lose!";
+}
+
+function draw(userChoice, computerChoice){
+   console.log("DRAW!")
+   result_p.innerHTML = userChoice + " equals to " + computerChoice +". It's a draw.";
+}
+
 function getComputerChoice(){
-   const choices = ['rock', 'paper', 'scissor'];
+   const choices = ['Rock', 'Paper', 'Scissor'];
    const randomNum = Math.floor(Math.random() * 3 )//generating rounded random number between 0..2
    return choices[randomNum];
 }
@@ -44,15 +65,15 @@ function getComputerChoice(){
 
 function main(){
    rock_div.addEventListener('click', function(){
-      game("rock");
+      game("Rock");
    })
    
    paper_div.addEventListener('click', function(){
-      game("paper");
+      game("Paper");
    })
    
    scissor_div.addEventListener('click', function(){
-      game("scissor");
+      game("Scissor");
    })
 }
 
